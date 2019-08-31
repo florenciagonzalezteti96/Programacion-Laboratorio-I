4f2f7f4eb1f67b3c2299d4ce28ea55eb7f5bb79b
alumno <alumno>
2019-08-31 12:07:05 -0300

    Agregue funciones a biblioteca para liberar espacio en main

--- Trabajo-Practico-I/Operation.c
+++ Trabajo-Practico-I/Operation.c
@@ -51,12 +51,12 @@ int  get_factorial(float number)
 }
 
 int validate (int variable_to_validate){
-    int retorno = 0;
+    int return_value = 0;
     if(variable_to_validate != 0){
         printf("Ha ocurrido un error en el programa. Por favor, ingrese de nuevo los datos.");
-        retorno = 1;
+        return_value = 1;
     }
-    retorno = 0;
+    return return_value;
 }
 
 int get_Option_Int(){
@@ -85,7 +85,7 @@ int validate_no_zero(int number_entered){
 
 int show_calculator(int first_number, int second_number){
     int operation=get_Int();
-    show_message("Ingrese 1 para suma, 2 para resta, 3 para division, 4 para multiplicacion, 5 para factorial.");
+    show_message("Ingrese 1 para sumar %d + %d.\nIngrese 2 para restar %d - %d.\nIngrese 3 para dividir %d / %d.\nIngrese 4 para multiplicar %d * %d.\nIngrese 5 para realizar el factorial de d! y d!.\n", first_number, second_number);
     switch(operation){
     case 1:
         add_numbers(first_number, second_number);
@@ -104,21 +104,19 @@ int show_calculator(int first_number, int second_number){
         get_factorial(second_number);
         break;
     }
+    return 0;
 }
 
 int show_option_menu(){
-
     int option;
     int flag=0;
     int flag_two=0;
     int first_number;
     int second_number;
     int option_continue=='s';
-
     do{
         option = get_Option_Int();
-        switch(option)
-        {
+        switch(option){
             case 1:
                 first_number=get_Int();
                 flag=1;
@@ -136,7 +134,11 @@ int show_option_menu(){
                         show_message("No ha ingresado un segundo operando. Seleccione 2.\n");
                     }
                     else{
-                        show_calculator();
+                        if(show_calculator()!=0){
+                           show_message("Ha ocurrido un error.");
+                           }else{
+                                show_calculator();
+                           }
                     }
                 }
             case 4:
